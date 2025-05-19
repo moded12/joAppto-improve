3cbf99091f0dd3796e62942e72bde92f21f9119e
Mohammad <162719285+moded12@users.noreply.github.com>
2025-05-19 09:08:49 +0300

    Delete lib directory

--- lib/apiConstants.dart
+++
@@ -1,4 +0,0 @@
-String APP_BASE_URL = 'https://www.shneler.com/mualim_al_aktarooni/api_v1/';
-String IMG_BASE_URL = 'https://www.shneler.com/e-learning';
-String DASHBOARD_API = APP_BASE_URL+"mualim_al_aktarooni_ios";
-int showAds = 0;
